b83fb3835205545bae443aa36a46b26fd3e39e60
@intelguasoft <hnrdiaz@gmail.com>
2019-10-25 20:39:15 -0600

    Se agrega el listado de paginacion

--- resources/views/multas/listar.blade.php
+++ resources/views/multas/listar.blade.php
@@ -15,6 +15,8 @@
         <div class="panel panel-default">
             <div class="panel-heading">Vehículo: <strong>P-768BCX</strong></div>
             <div class="panel-body">
+                <div class="jumbotron">
+                    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>                </div>
                 <ul class="list-group">
                     <li class="list-group-item">
                         <span class="badge">El Estor</span>
@@ -51,4 +53,23 @@
     @endforelse
 </div>
 {{ $multas->render() }}
+@stop
+
+
+@section('js')
+<script>
+    $(function() {
+
+    });
+</script>
+@stop
+
+@section('css')
+<style>
+div.panel div.panel-body .jumbotron
+{
+padding: 5px;
+margin-bottom: 2px;
+}
+</style>
 @stop
